ab8f3bd122b930e34264161312099c91c1d20d07
Dzuba <tzar4eg@gmail.com>
2019-09-22 11:51:15 +0300

    Добавлены сообщения об ошибке

--- src/app/components/dropzone/dropzone.component.ts
+++ src/app/components/dropzone/dropzone.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import { THIS_EXPR } from "@angular/compiler/src/output/output_ast";
 
 @Component({
   selector: "app-dropzone",
@@ -6,13 +7,23 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./dropzone.component.less"]
 })
 export class DropzoneComponent implements OnInit {
+  public errorMessage = "";
+  public warningMessage = false;
   constructor() {}
-  testImagesArray = [];
   ngOnInit() {}
   onFileChange(event: FileList): void {
     const images: File[] = Array.prototype.filter.call(event, (file: File) => {
       return file.type.match("image");
     });
-    this.testImagesArray = images;
+    if (!images.length) {
+      this.errorMessage = "Загрузить можно только изображения!";
+      return;
+    }
+    if (images.length !== event.length) {
+      this.warningMessage = true;
+      this.errorMessage = `<div>Файлов: ${event.length}, Изображений: ${images.length}.</div><div>Будут загружены только изображения!</div>`;
+      return;
+    }
+    this.errorMessage = "";
   }
 }
