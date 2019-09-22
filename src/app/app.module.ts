424ae067241de0530f5c4f48da5a33de8b139a5d
Dzuba <tzar4eg@gmail.com>
2019-09-22 09:13:18 +0300

    Добавлен компонент дропзоны

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { DropzoneComponent } from './components/dropzone/dropzone.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DropzoneComponent
   ],
   imports: [
     BrowserModule
