f15192aadedfffd40b686a6700cbaed4fb7d79f9
Dzuba <tzar4eg@gmail.com>
2019-09-22 14:32:41 +0300

    Добавил эвент эммиторы и transition на кнопки

--- src/app/components/image-viewer/selected-image/selected-image.component.ts
+++ src/app/components/image-viewer/selected-image/selected-image.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output } from '@angular/core';
+import { EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-selected-image',
@@ -7,9 +8,20 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class SelectedImageComponent implements OnInit {
   @Input() image: string;
-  
+  @Output() forward: EventEmitter<any> = new EventEmitter();
+  @Output() backward: EventEmitter<any> = new EventEmitter();
   constructor() { }
   ngOnInit() {
   }
+  
+  onForward(): void {
+    console.log('forward');
+    this.forward.emit();
+  }
+  onBackward(): void {
+    console.log('backward');
 
+    this.backward.emit();
+  }
+  
 }
