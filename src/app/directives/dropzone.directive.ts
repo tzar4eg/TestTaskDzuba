698d86d32973a6a2533132b6d776b97951555d61
Dzuba <tzar4eg@gmail.com>
2019-09-22 10:30:22 +0300

    Реализована директива dropzone

--- src/app/directives/dropzone.directive.ts
+++ src/app/directives/dropzone.directive.ts
@@ -1,10 +1,38 @@
-import { Directive } from '@angular/core';
+import { Directive, Output, HostListener } from '@angular/core';
+import { EventEmitter } from '@angular/core';
 
 @Directive({
   selector: '[appDropzone]'
 })
 export class DropzoneDirective {
+  @Output() fileDropped: EventEmitter<FileList> = new EventEmitter<FileList>();
+  @Output() hoveredWithFile: EventEmitter<boolean> = new EventEmitter<boolean>();
 
   constructor() { }
 
+  @HostListener('dragover', ['$event'])
+  onDragOver(event: DragEvent): void {
+    event.preventDefault();
+    event.stopPropagation();
+    this.hoveredWithFile.emit(true);
+    
+  }
+  @HostListener('dragleave', ['$event'])
+  onDragLeave(event: DragEvent): void{
+    event.preventDefault();
+    event.stopPropagation();
+    this.hoveredWithFile.emit(false);
+
+  }
+
+  @HostListener('drop', ['$event'])
+  onDrop(event: DragEvent): void {
+    event.preventDefault();
+    event.stopPropagation();
+    if (event.dataTransfer.files.length === 0) {
+      return;
+    }
+    this.hoveredWithFile.emit(false);    
+    this.fileDropped.emit(event.dataTransfer.files);
+  }
 }
