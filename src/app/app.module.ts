da8e63398ed7cb4f0443afc2b433da2824863e36
Dzuba <tzar4eg@gmail.com>
2019-09-22 12:07:38 +0300

    Создан компонент для image вьювера

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { DropzoneComponent } from './components/dropzone/dropzone.component';
 import { DropzoneDirective } from './directives/dropzone.directive';
+import { ImageViewerComponent } from './components/image-viewer/image-viewer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DropzoneComponent,
-    DropzoneDirective
+    DropzoneDirective,
+    ImageViewerComponent
   ],
   imports: [
     BrowserModule
