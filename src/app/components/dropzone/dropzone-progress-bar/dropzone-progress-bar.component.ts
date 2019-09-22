7c8422939ce00df7d48dc0aedcc9ffb5bffec542
Dzuba <tzar4eg@gmail.com>
2019-09-22 22:15:21 +0300

    Реализована дропзона

--- src/app/components/dropzone/dropzone-progress-bar/dropzone-progress-bar.component.ts
+++ src/app/components/dropzone/dropzone-progress-bar/dropzone-progress-bar.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpEventType } from '@angular/common/http';
 
 @Component({
   selector: 'app-dropzone-progress-bar',
@@ -6,10 +8,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dropzone-progress-bar.component.less']
 })
 export class DropzoneProgressBarComponent implements OnInit {
-
+  @Input() uploadingStream: {stream: Observable<any>, fileName: string};
+  @Input() progress: number = 0;
   constructor() { }
 
   ngOnInit() {
+    this.uploadingStream.stream.subscribe(event => {
+      if(event.type === HttpEventType.UploadProgress)  {
+        this.progress = Math.round(event.loaded / event.total)
+            }
+
+    })
   }
 
 }
--- src/app/components/dropzone/dropzone.component.ts
+++ src/app/components/dropzone/dropzone.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { Observable } from 'rxjs';
 
 @Component({
   selector: "app-dropzone",
@@ -9,33 +10,41 @@ import { HttpClient, HttpHeaders } from "@angular/common/http";
 export class DropzoneComponent implements OnInit {
   public errorMessage = "";
   public warningMessage = false;
+  public images: File[];
+  public filesToUpload: {stream: Observable<any>, fileName: string}[];
+
   constructor(private _http: HttpClient) {}
   ngOnInit() {}
   onFileChange(event: FileList): void {
-    const images: File[] = Array.prototype.filter.call(event, (file: File) => {
+    this.images = Array.prototype.filter.call(event, (file: File) => {
       return file.type.match("image");
     });
-    if (!images.length) {
+    if (!this.images.length) {
       this.errorMessage = "Загрузить можно только изображения!";
       return;
     }
-    if (images.length !== event.length) {
+    if (this.images.length !== event.length) {
       this.warningMessage = true;
-      this.errorMessage = `<div>Файлов: ${event.length}, Изображений: ${images.length}.</div><div>Будут загружены только изображения!</div>`;
+      this.errorMessage = `<div>Файлов: ${event.length}, Изображений: ${this.images.length}.</div><div>Будут загружены только изображения!</div>`;
+      this.filesToUpload = this.uploadFiles(this.images);
       return;
     }
-    
-    this.upploadFiles(images)
+    this.filesToUpload = this.uploadFiles(this.images);
     this.errorMessage = "";
   }
+  uploadFiles(images: File[]) {
+    return Array.prototype.map.call(images, ((image: File) => {
+      return {stream: this.uploadFile(image), fileName: image.name};
+    }))
+  }
 
-  
-
-  upploadFiles(files: File[]) {
+  uploadFile(file: File): Observable<any> {
     let formData = new FormData();
-    let url = "http://localhost:4100/upload"; 
-    formData.append('image', files[0], files[0].name)
-     
-    this._http.post(url, formData).subscribe((resp: {fileName: string}) => console.log(resp.fileName))
+    let url = "http://localhost:4100/upload";
+    formData.append("image", file, file.name);
+    return this._http.post(url, formData, {
+      reportProgress: true,
+      observe: "events"
+    });
   }
 }
