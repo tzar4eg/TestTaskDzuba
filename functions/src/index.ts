1606895c9ba3762554ec1285ca7aa2476b2ea05e
Dzuba <tzar4eg@gmail.com>
2019-09-22 14:55:02 +0300

    Добавлен метод для загрузки файлов

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,4 +1,4 @@
-import * as functions from 'firebase-functions';
+import * as functions from "firebase-functions";
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
@@ -6,3 +6,11 @@ import * as functions from 'firebase-functions';
 // export const helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+
+exports.onImageUpload = functions.https.onRequest((req, res) => {
+  if (req.method !== "POST") {
+    return res.status(500).json({
+      message: "Not allowed"
+    });
+  }
+});
