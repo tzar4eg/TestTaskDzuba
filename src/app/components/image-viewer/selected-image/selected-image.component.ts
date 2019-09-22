f0bed6ca86397bc79b028ca4f837e235a9d2c407
Dzuba <tzar4eg@gmail.com>
2019-09-22 14:35:05 +0300

    Реализованы эвенты на просмотр

--- src/app/components/image-viewer/selected-image/selected-image.component.ts
+++ src/app/components/image-viewer/selected-image/selected-image.component.ts
@@ -1,27 +1,22 @@
-import { Component, OnInit, Input, Output } from '@angular/core';
-import { EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, Output } from "@angular/core";
+import { EventEmitter } from "@angular/core";
 
 @Component({
-  selector: 'app-selected-image',
-  templateUrl: './selected-image.component.html',
-  styleUrls: ['./selected-image.component.less']
+  selector: "app-selected-image",
+  templateUrl: "./selected-image.component.html",
+  styleUrls: ["./selected-image.component.less"]
 })
 export class SelectedImageComponent implements OnInit {
   @Input() image: string;
   @Output() forward: EventEmitter<any> = new EventEmitter();
   @Output() backward: EventEmitter<any> = new EventEmitter();
-  constructor() { }
-  ngOnInit() {
-  }
-  
+  constructor() {}
+  ngOnInit() {}
+
   onForward(): void {
-    console.log('forward');
     this.forward.emit();
   }
   onBackward(): void {
-    console.log('backward');
-
     this.backward.emit();
   }
-  
 }
