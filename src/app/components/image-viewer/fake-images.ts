b542c39e949db63b0dcd0a4b63665f12632ef747
dzyuba <dzyuba@wiseadvice.ru>
2019-09-23 08:50:00 +0300

    Remove mocked data

--- src/app/components/image-viewer/fake-images.ts
+++
@@ -1,8 +0,0 @@
-export let fakeImages = [
-    'assets/photo_2019-09-22_12-03-07.jpg',
-    'assets/photo_2019-09-22_12-03-31.jpg',
-    'assets/photo_2019-09-22_12-03-33.jpg',
-    'assets/photo_2019-09-22_12-03-35.jpg',
-    'assets/photo_2019-09-22_12-03-38.jpg',
-    'assets/photo_2019-09-22_12-03-41.jpg'
-]
